05c2d0d6c8b8d9b2a4e8e1d9d6e969eb95f0e594
varunsaini96 <139777508+varunsaini96@users.noreply.github.com>
2026-06-06 19:58:31 +0530

    Change database password in JDBCLearning.java
    Updated the database password for connection.

--- src/JDBC/JDBCLearning.java
+++ src/JDBC/JDBCLearning.java
@@ -14,7 +14,7 @@ public class JDBCLearning {
 
         String url = "jdbc:mysql://localhost:3306/jdbclearning";
         String user = "root";
-        String password = "Alex@3215";
+        String password = "12345";
         Connection connect = DriverManager.getConnection(url, user, password);
 
         //Creating Statement
